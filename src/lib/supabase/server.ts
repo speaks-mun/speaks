04d3d8614c9e3721b23667990402796861d864db
Priyanka Datta <98142962+shrekingmyhead@users.noreply.github.com>
2025-08-26 03:17:50 +0530

    server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,27 +1,27 @@
-import { createServerClient } from "@supabase/ssr"
-import { cookies } from "next/headers"
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
 
-export function createServerSupabaseClient() {
-  const cookieStore = cookies()
+export function createClient() {
+	const cookieStore = cookies();
 
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {
-            // ignore in server components
-          }
-        },
-      },
-    }
-  )
+	return createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll();
+				},
+				setAll(cookiesToSet) {
+					try {
+						cookiesToSet.forEach(({ name, value, options }) =>
+							cookieStore.set(name, value, options)
+						);
+					} catch {
+						// Ignore if running in a Server Component
+					}
+				},
+			},
+		}
+	);
 }
